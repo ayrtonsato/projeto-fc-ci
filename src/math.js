0e1f1d9053eee9e4cac38075dc55338505f049f7
Ayrton Sato <ayrton_sato@hotmail.com>
2021-03-09 16:07:08 -0300

    feat: add tests

---
+++ src/math.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+	return a + b;
+}
+
+const minus = (a, b) => {
+	return a - b;
+}
+
+const times = (a, b) => {
+	return a * b;
+}
+
+const div = (a, b) => {
+	return a / b;
+}
+
+module.exports = {
+	sum,
+	minus,
+	times,
+	div
+}
---
+++ src/math.test.js
@@ -0,0 +1,19 @@
+const math = require('./math')
+
+describe('Math', () => {
+	test('sum', () => {
+		expect(math.sum(1, 1)).toBe(2)
+	})
+
+	test('minus', () => {
+		expect(math.minus(1, 1)).toBe(0)
+	})
+
+	test('times', () => {
+		expect(math.times(1, 1)).toBe(1)
+	})
+
+	test('div', () => {
+		expect(math.div(2, 2)).toBe(1)
+	})
+})
